03db8a41e1daedda755b2a359dd8b9212bed908b
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 23:06:46 -0700

    Create orderOfOptions.java

---
+++ orderOfOptions.java
@@ -0,0 +1,14 @@
+public class Operations {
+	public static void main(String[] args) { 
+
+    int expression1 = 5 % 2 - (4 * 2 - 1);
+    System.out.println(expression1);
+
+    int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+    System.out.println(expression2);
+
+    int expression3 = 5 * 4 % 3 - 2 + 1;
+    System.out.println(expression3);
+
+	}
+}
