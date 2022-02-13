b515f7441380a021ef7391ef015d089796bffd43
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 22:57:07 -0700

    Create myprofile.java

---
+++ myprofile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Slick_Torpedo";
+    int age = 15;
+    double desiredSalary = 100.01;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
