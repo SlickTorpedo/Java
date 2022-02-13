8ef80282d84866500255aa6c98a9ce892c4238f1
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-13 12:36:25 -0700

    Create simpleIfThen.java

---
+++ simpleIfThen.java
@@ -0,0 +1,12 @@
+public class Order {
+  
+  public static void main(String[] args) {
+    
+    double itemCost = 30.99;
+    
+    // Write an if-then statement:
+    if (itemCost > 24.00) System.out.println("High value item!");
+    
+  }
+  
+}
