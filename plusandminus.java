8cb1c158e5831add1bcf7e05d3f5c5fa19fa5550
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 23:01:01 -0700

    Create plusandminus.java

---
+++ plusandminus.java
@@ -0,0 +1,10 @@
+public class PlusAndMinus {
+	public static void main(String[] args) {   
+		int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    System.out.println(animalsInZoo);
+    int numZebrasAfterTrade = animalsInZoo - 6;
+    System.out.println(numZebrasAfterTrade);
+	}
+}
