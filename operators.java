dbd056d787a84a1e26fff1c62c195403370a9b2f
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 23:06:01 -0700

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,9 @@
+public class BakeSale {
+	public static void main(String[] args) {   
+		int numCookies = 17;
+    numCookies -= 3;
+    numCookies /= 2;
+    // Add your code above
+    System.out.println(numCookies);
+	}
+}
