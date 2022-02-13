424fb656d19dc2dd16c69061ea2a82d4b2b23b7a
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-13 13:14:21 -0700

    Create nestedConditions.java

---
+++ nestedConditions.java
@@ -0,0 +1,50 @@
+public class Order {
+  boolean isFilled;
+  double billAmount;
+  String shipping;
+  String couponCode;
+  
+  public Order(boolean filled, double cost, String shippingMethod, String coupon) {
+		if (cost > 24.00) {
+      System.out.println("High value item!");
+    }
+    isFilled = filled;
+    billAmount = cost;
+    shipping = shippingMethod;
+    couponCode = coupon;
+  }
+  
+  public void ship() {
+    if (isFilled) {
+      System.out.println("Shipping");
+      System.out.println("Shipping cost: " + calculateShipping());
+    } else {
+      System.out.println("Order not ready");
+    }
+  }
+  
+  public double calculateShipping() {
+    if (shipping.equals("Regular")) {
+      return 0;
+    } else if (shipping.equals("Express")) {
+      if (couponCode.equals("ship50")) {
+        return .85;
+      } else {
+        return 1.75;
+      }
+
+      
+    } else {
+      return .50;
+    }
+ 	}
+  
+  public static void main(String[] args) {
+    // do not alter the main method!
+    Order book = new Order(true, 9.99, "Express", "ship50");
+    Order chemistrySet = new Order(false, 72.50, "Regular", "freeShipping");
+    
+    book.ship();
+    chemistrySet.ship();
+  }
+}
