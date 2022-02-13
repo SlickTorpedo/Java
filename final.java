35e75a7df278db628543c924d46898629cffaf88
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 23:23:13 -0700

    Create final.java

---
+++ final.java
@@ -0,0 +1,8 @@
+public class Final {
+	public static void main(String[] args) { 
+      final double pi = 3.14;
+      System.out.print(pi);
+      //pi = 1313131;
+      //Cannot be changed because it's a FINAL variable!
+	}
+}
