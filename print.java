60c2ee74a9d0cbbb0b622796d925a5e1ee37d597
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 22:33:32 -0700

    Create print.java

---
+++ print.java
@@ -0,0 +1,9 @@
+public class HideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("One...");
+    System.out.println("ready or not, here I come!");
+  }
+}
