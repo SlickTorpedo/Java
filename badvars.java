6f4d786d085b0cbdc30a6767c2f8c2e170d657e7
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 22:55:22 -0700

    Create badvars.java

---
+++ badvars.java
@@ -0,0 +1,14 @@
+public class BadNames {
+	public static void main(String[] args) {   
+		String stName = "Samira";
+    String blah = "Smith";
+    String com = "samira@google.com";
+    int salaryexpectation = 100000;
+    int year_of_birth = 1955;
+    
+    System.out.println("The program runs!");
+	}
+}
+
+//Variables can only start with $ or _
+//no other symbols will work or else it will compile with errors
