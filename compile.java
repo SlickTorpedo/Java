3f66215a08792304510fccdca4252d3dd3a65ada
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 22:36:34 -0700

    Create compile.java

---
+++ compile.java
@@ -0,0 +1,14 @@
+public class Compiling {
+  public static void main(String[] args) {
+    
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+
+    System.out.println("Programming is... fun!");
+    
+  }
+}
+
+//javac Compiling.java - compiles
+//ls - views files in directory
