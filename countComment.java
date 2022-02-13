7d804e31a63f38cc03ab37cac6f3e78b3db10881
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 22:46:09 -0700

    Create countComment.java

---
+++ countComment.java
@@ -0,0 +1,15 @@
+//This is the class declaration
+public class CountComment {
+  //This is the main method that runs when you compile
+	public static void main(String[] args) {
+    //This is where you will define your variable
+    int numComments = 6;
+    System.out.println(numComments);
+    //This is where you will print your variable
+    
+	}
+  
+  //This is the end of the class
+}
+
+//This is outside the class
