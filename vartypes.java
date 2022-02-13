d0362a86e9564de41a435150a8a74987b5c9ecbf
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-12 22:53:43 -0700

    Create vartypes.java

---
+++ vartypes.java
@@ -0,0 +1,9 @@
+public class Mess {
+	public static void main(String[] args) {   
+		int year = 2001;
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
+	}
+}
