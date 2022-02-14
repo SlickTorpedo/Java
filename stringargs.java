c06f2bfbceac7d48b80919972f40b964d2b28639
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-14 09:35:20 -0600

    Create stringargs.java

---
+++ stringargs.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Newsfeed {
+  
+  String[] topics;
+  
+  public Newsfeed(String[] initialTopics) {
+		topics = initialTopics;
+  }
+  
+  public static void main(String[] args) {
+		Newsfeed feed;
+    if (args[0].equals("Human")) {
+      
+      //topics for a Human feed:
+      String[] humanTopics = {"Politics", "Science", "Sports", "Love"};
+			feed = new Newsfeed(humanTopics);
+      
+    } else if(args[0] == "Robot") {
+      
+      //topics for a Robot feed:
+      String[] robotTopics = {"Oil", "Parts", "Algorithms", "Love"};
+      feed = new Newsfeed(robotTopics);
+      
+    } else {
+      String[] genericTopics = {"Opinion", "Tech", "Science", "Health"};
+      feed = new Newsfeed(genericTopics);
+    }
+        
+    System.out.println("The topics in this feed are:");
+    System.out.println(Arrays.toString(feed.topics));
+  }
+}
