39e6d0160f40a8f9c9e3032c27018c74a444935f
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-14 12:00:03 -0600

    Create addingItemsToArray.java

---
+++ addingItemsToArray.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    // Add more to-dos here:
+    String toDo2 = "Clean House";
+    String toDo3 = "Mop Floors";
+    
+    // Add to-dos to toDoList
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+    
+    System.out.println(toDoList);
+      
+    
+  }
+  
+}
