dcd9ff7f6f7b301cdad029007d9cfd0a50412ec1
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-15 09:13:44 -0600

    Create whilewithintegers.java

---
+++ whilewithintegers.java
@@ -0,0 +1,16 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    // initialize cupsOfCoffee
+    int cupsOfCoffee = 1;
+    // add while loop with counter
+    while (cupsOfCoffee <= 100) {
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      cupsOfCoffee++;
+    }
+        
+    
+  }
+  
+}
