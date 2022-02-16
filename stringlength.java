de1322a4952a73821b0bea99377201685401d4cc
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-15 19:50:24 -0600

    Create stringlength.java

---
+++ stringlength.java
@@ -0,0 +1,22 @@
+public class TweetCounter {
+  
+	public static void main(String[] args) {
+    
+    String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";  
+
+    /*
+    int tweetl = tweet.length();
+    if(tweetl >= 280) {
+      System.out.println(tweetl);
+      System.out.println("Posted!");
+    } else {
+      System.out.println("Sorry, your tweet is too long!");
+      int av = tweetl - 280;
+      System.out.println("Make it " + av + " characters shorter!");
+      System.out.println(tweetl);
+    }*/
+    System.out.println(tweet.length());
+    
+  }
+  
+}
