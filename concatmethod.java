202f514c1d3c58d3785a34c494ae3ccfe4b02481
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-15 19:54:12 -0600

    Create concatmethod.java

---
+++ concatmethod.java
@@ -0,0 +1,14 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "Philip";  
+    String lastName = "Ehrbright";
+
+    // What's the full name?
+    System.out.println(firstName.concat(" " + lastName));
+    
+  }
+  
+}
