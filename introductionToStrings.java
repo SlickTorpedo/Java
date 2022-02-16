5593a6c433c93abd85368782d0eeefa570b3d818
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-15 19:45:19 -0600

    Create introductionToStrings.java

---
+++ introductionToStrings.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+  
+	public static void main(String[] args) {
+    
+    String str = "Hello, World!";
+    
+    // Examples:
+
+    System.out.println(str.length());
+    
+    System.out.println(str.substring(4));
+    
+    System.out.println(str.toUpperCase());
+    
+  }
+  
+}
