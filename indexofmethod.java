0f238fe1cbde1531fe0c47944e1dc9ed0e525dc8
Slick Torpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-02-15 20:00:59 -0600

    Create indexofmethod.java

---
+++ indexofmethod.java
@@ -0,0 +1,12 @@
+public class MorseCode {
+  
+  public static void main(String[] args) {
+    
+    String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+    System.out.println(code.indexOf(". .- -"));
+    
+    //Still don't really understand how this works lol
+  }
+  
+}
